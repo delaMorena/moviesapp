09bb2975ea94bdbef2954b8ada434b7114209910
martiby <martadmdo@gmail.com>
2021-03-25 13:40:19 +0100

    initial commit

---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,15 @@
+import movies from "./movies.json"
+import { MovieCard } from "./MovieCard"
+import styles from "./MoviesGrid.module.css"
+
+
+export function MoviesGrid() {
+    console.log(movies);
+  return (
+    <ul className={styles.moviesGrid}>
+        {movies.map((movie) => (
+           <MovieCard   key={movie.id} movie={movie} />
+         ))}
+    </ul>
+  );
+}
